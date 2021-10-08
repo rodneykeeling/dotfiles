65d5246ad246cc83a9dbf35d1c88bdd2c8e5a2fd
Rodney Keeling <rodneykeeling@gmail.com>
2021-10-08 08:46:02 -0700

    Remove unused Python class finder mapping

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,7 +13,6 @@ map('n', 'n', 'nzz', { noremap = true })
 map('n', 'N', 'Nzz', { noremap = true })
 
 -- <leader>c to print current python class name
-map('n', '<leader>c', '<cmd><c-u>echo trim(getline(search("^class", "bnW")))<cr>', {})
 map('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<cr>', {})
 map('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<cr>', {})
 map('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', {})
