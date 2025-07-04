03a17d47963e703e0f1b57378198d9fb7e61a90d
= <rodneykeeling@proton.me>
2025-07-04 13:42:30 -0600

    Update for wayland/sway config

--- sxhkd/.config/sxhkd/hipster.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-nodes=( $(bspc query -N -d focused) )
-bspc node ${nodes[1]} -t floating
-bspc node ${nodes[1]} -v -475 0
-bspc node ${nodes[2]} -t floating
-bspc node ${nodes[2]} -v 475 0
